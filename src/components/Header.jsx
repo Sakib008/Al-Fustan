95131b441ac1ec9dd4fd95fcc42bbcf00ae178ca
Sakib008 <mdshakib1902@gmail.com>
2024-10-26 09:52:23 +0530

    Feat : make the header with css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+
+import "../Style/header.css";
 import logo from "../assets/logo2.png";
 import searchIcon from "../assets/search-icon.svg";
 import wishListIcon from "../assets/wishlist-icon.svg";
@@ -6,27 +8,36 @@ import cartIcon from "../assets/cart icon.svg";
 
 export function Header() {
   return (
-    <div className="flex justify-center mt-1">
-      <nav className="flex w-10/12 justify-between">
-        <img src={logo} alt="al-fushan" className="logo w-44 " />
-        <div className="header w-4/6 flex justify-between text-center">
-          <div className="left-header w-44 ">
-            <NavLink to="/man" className='text-4xl mr-12'>Men</NavLink>
-            <NavLink to="/women" className='text-4xl'>Women</NavLink>
-          </div>
-          <div className="right-header w-4/6 flex justify-end ml">
-            <NavLink>
-              <img src={searchIcon} alt="Search" className="search w-16 mr-20" />
-            </NavLink>
-            <NavLink>
-              <img src={wishListIcon} alt="wishList" className="wishList w-16 ml-10" />
-            </NavLink>
-            <NavLink>
-              <img src={cartIcon} alt="cartIcon" className="cartIcon w-16 ml-10" />
-            </NavLink>
+    <div className='container mx-auto py-3 pl-12 pr-12'>
+      <header className="header flex w-4/5 my-auto mx-auto justify-between ">
+        <a href="/" className="logo">
+          <img src={logo} alt="Al Fushan" className="w-28" />
+        </a>
+        <nav className="navbar rounded-3xl border-x-indigo-700 w-2/5 justify-center font-extrabold py-1 text-4xl my-auto flex">
+          <ul className="flex">
+            <li className="px-8">
+              <NavLink>Men</NavLink>
+            </li>
+            <li className="px-8">
+              <NavLink>Women</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex w-1/6 my-auto justify-between">
+          <a href="/search" className="headerIcon ">
+            <img src={searchIcon} alt="Search " className="w-16" />
+          </a>
+          <div className="icon w-3/5 flex justify-between">
+            <a href="/" className="headerIcon">
+              <img src={wishListIcon} alt="WishList" className="w-16" />
+            </a>
+
+            <a href="/" className="headerIcon">
+              <img src={cartIcon} alt="Cart" className="w-16" />
+            </a>
           </div>
         </div>
-      </nav>
+      </header>
     </div>
   );
 }
