92ebc49565e36338119ab25ff58b7e5b2f844bb8
Sakib008 <mdshakib1902@gmail.com>
2024-10-27 13:14:12 +0530

    feat : add product list rough structure

---
+++ src/Pages/ProductList.jsx
@@ -0,0 +1,13 @@
+export function ProductList(){
+    return(
+        <div className="product-page h-[100vh]">
+            <div className="filter-section w-2/12 bg-slate-500">
+
+            </div>
+            <div className="product-section">
+
+            </div>
+
+        </div>
+    )
+}
