48fa7c34cba6568156de71104146c00b2557def2
Sakib008 <mdshakib1902@gmail.com>
2024-10-27 13:06:18 +0530

    fix : added router and Product list page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import {Route,Routes} from 'react-router-dom'
+
 import "./App.css";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Home } from "./Pages/Home";
+import {ProductList} from './Pages/ProductList';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/product-list' element={<ProductList/>} />
+      </Routes>
       <Home/>
       <Footer/>
     </div>
